a9c2898bd3c053ca46bb6b4866e4fee8bd135638
torinunna <torinunna9588@gmail.com>
2022-08-26 23:40:52 +0900

    implement changing text and background colors methods in SettingVC

--- LEDBoard/SettingViewController.swift
+++ LEDBoard/SettingViewController.swift
@@ -16,7 +16,7 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var greenButton: UIButton!
     
     @IBOutlet weak var blackButton: UIButton!
-    @IBOutlet weak var mintButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
     
     override func viewDidLoad() {
@@ -24,13 +24,37 @@ class SettingViewController: UIViewController {
     }
     
     @IBAction func textColorPressed(_ sender: UIButton) {
+        if sender == self.yellowButton {
+            self.changeTextColor(color: .yellow)
+        } else if sender == self.purpleButton {
+            self.changeTextColor(color: .purple)
+        } else {
+            self.changeTextColor(color: .green)
+        }
     }
     
     @IBAction func backgroudColorPressed(_ sender: UIButton) {
+        if sender == self.blackButton {
+            self.changeBackgroundColor(color: .black)
+        } else if sender == self.orangeButton {
+            self.changeBackgroundColor(color: .orange)
+        } else {
+            self.changeBackgroundColor(color: .blue)
+        }
     }
     
     @IBAction func savePressed(_ sender: UIButton) {
     }
     
+    private func changeTextColor(color: UIColor) {
+        self.yellowButton.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleButton.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenButton.alpha = color == UIColor.green ? 1 : 0.2
+    }
     
+    private func changeBackgroundColor(color: UIColor) {
+        self.blackButton.alpha = color == UIColor.black ? 1 : 0.2
+        self.orangeButton.alpha = color == UIColor.orange ? 1 : 0.2
+        self.blueButton.alpha = color == UIColor.blue ? 1 : 0.2
+    }
 }
